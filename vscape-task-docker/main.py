a29ec0b74e2ead88352d3d0a9c78640cf5a7ee6d
Zvenyika Gomo <zvenyika.gomo@excelian.com>
2015-01-30 15:31:40 +0000

    * made main.py accept container and cmd as cmdline args

--- vscape-task-docker/main.py
+++ vscape-task-docker/main.py
@@ -1,27 +1,38 @@
 from random import randint
 from docker import Client
 from container import DockerTask,Executor
+import sys
 import unittest
 import psutil
 c = Client(base_url='unix://var/run/docker.sock')
 
-# google style guidelines : https://google-styleguide.googlecode.com/svn/trunk/pyguide.html#Nested/Local/Inner_Classes_and_Functions 
-#t=DockerTask(container,cmd)
-#t._type="docker"
-#Executor.run(t)
-#	register t 
-#	execute t 
-#	deregister t 
+
+if len(sys.argv) < 1:
+    print "Usage: executor container 'command'"
+    import sys;sys.exit(0)
+
+client =  Client(base_url='unix://var/r'
+                          'un/docker.sock')
+
+container = sys.argv[1]
+cmd= sys.argv[2]
 
 
+if len(sys.argv) > 2:
+	container = sys.argv[1] 
 
+	
+print sys.argv
 
-client =  Client(base_url='unix://var/run/docker.sock')
-cmd='sleep 5m'
 task=DockerTask("ubuntu:10.04",cmd)
 executor = Executor("serviceId&id=0",["service=executor","host=127.0.0.1"])
-# executor.register("etcd://")
-executor.runTask("ubuntu:10.04","sleep 5m")
-executor.waitForExit()
+try:
+    executor.register("etcd://10.28.1.159:5001")
+    executor.runTask(container,cmd)
+    executor.waitForExit()
+    executor.shutdown()
+except Exception,e:
+    print "Something bad happened : %s " % str(e)
+    executor.shutdown()
 
 
